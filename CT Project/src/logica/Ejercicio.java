476e30871d1ef29b14476dd160a919b1b401753b
JoseParada <josehall1@hotmail.com>
2013-11-02 21:06:06 -0500

    Añadida clase Rutina y Ejercicio

---
+++ CT Project/src/logica/Ejercicio.java
@@ -0,0 +1,39 @@
+package logica;
+import java.awt.Image;
+public class Ejercicio {
+	private int idEjercicio;
+	private String nombreEjercicio;
+	private String descripcionEjercicio;
+	private int puntajeBase;
+	private Image imagenEjercicio;
+	public Ejercicio(int idEjercicio, String nombreEjercicio,
+			String descripcionEjercicio, int puntajeBase, Image imagenEjercicio) {
+		super();
+		this.idEjercicio = idEjercicio;
+		this.nombreEjercicio = nombreEjercicio;
+		this.descripcionEjercicio = descripcionEjercicio;
+		this.puntajeBase = puntajeBase;
+		this.imagenEjercicio = imagenEjercicio;
+	}
+	public int getIdEjercicio() {
+		return idEjercicio;
+	}
+	public String getNombreEjercicio() {
+		return nombreEjercicio;
+	}
+	public String getDescripcionEjercicio() {
+		return descripcionEjercicio;
+	}
+	public int getPuntajeBase() {
+		return puntajeBase;
+	}
+	public Image getImagenEjercicio() {
+		return imagenEjercicio;
+	}
+	@Override
+	public String toString() {
+		return idEjercicio + "-"
+				+ nombreEjercicio + "\n"
+				+ descripcionEjercicio + "\n>" + puntajeBase;
+	}
+}
