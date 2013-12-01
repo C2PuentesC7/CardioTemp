83efa01f952ed87d4b43328cf70225906670ed75
JoseParada <josehall1@hotmail.com>
2013-11-30 17:22:34 -0800

    Clase logro, que contiene la estructura de un logro a desbloquear por el usuario al hacer cierta actividad

---
+++ CT Project/src/Logica/Logro.java
@@ -0,0 +1,42 @@
+package Logica;
+
+import java.io.Serializable;
+
+public class Logro implements Serializable{
+	private int idLogro;
+	private String descripcionLogro;
+	private int puntajeLogro;
+	private Ejercicio ejercicio;
+	private boolean cumplido;
+	public Logro(int idLogro, String descripcionLogro, int puntajeLogro,
+			Ejercicio ejercicio) {
+		super();
+		this.idLogro = idLogro;
+		this.descripcionLogro = descripcionLogro;
+		this.puntajeLogro = puntajeLogro;
+		this.ejercicio = ejercicio;
+	}
+	public int getIdLogro() {
+		return idLogro;
+	}
+	public String getDescripcionLogro() {
+		return descripcionLogro;
+	}
+	public int getPuntajeLogro() {
+		return puntajeLogro;
+	}
+	public Ejercicio getEjercicio() {
+		return ejercicio;
+	}
+	public boolean isCumplido() {
+		return cumplido;
+	}
+	public void setCumplido(boolean cumplido) {
+		this.cumplido = cumplido;
+	}
+	@Override
+	public String toString() {
+		return idLogro + "-"
+				+ descripcionLogro + " > " + puntajeLogro + " Eje: " + ejercicio;
+	} 
+}
