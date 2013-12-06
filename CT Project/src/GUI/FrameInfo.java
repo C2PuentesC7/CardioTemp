f04972d64603c03ba12c65ebdcf7c1c9946aa2a6
C2PuentesC7 <camilopuentescur@hotmail.com>
2013-12-05 20:57:29 -0500

    Frame que sirve para mostrar la informacion de los tips o de los ejercicios

--- CT Project/src/GUI/FrameInfo.java
+++ CT Project/src/GUI/FrameInfo.java
@@ -1,19 +1,15 @@
 package GUI;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import java.awt.Window.Type;
 
 import javax.swing.JLabel;
 
 import java.awt.Font;
 
 import javax.swing.ImageIcon;
+import javax.swing.JRootPane;
 import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
@@ -26,74 +22,78 @@ import javax.swing.JScrollPane;
 
 public class FrameInfo extends JFrame implements ActionListener {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JLabel lblTipo;
 	private JLabel lblImagen;
 	private JTextArea txtrDescripcion;
+	private FrmPrincipal fp;
+	private FrmCronometro fc;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FrameInfo frame = new FrameInfo();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
+	 * @param fp 
+	 * @param fc 
 	 */
-	public FrameInfo() {
-		setType(Type.UTILITY);
+	public FrameInfo(FrmPrincipal fp, FrmCronometro fc) {
+		this.fp = fp;
+		this.fc = fc;
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 250, 400);
+		setIconImage(new ImageIcon(FrmPrincipal.class.getResource("/Imagenes/icono.png")).getImage());
+		setUndecorated(true);
+		getRootPane().setWindowDecorationStyle(JRootPane.NONE);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		lblTipo = new JLabel("Tipo");
-		lblTipo.setForeground(new Color(255, 255, 255));
-		lblTipo.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTipo.setFont(new Font("Agency FB", Font.PLAIN, 27));
 		lblTipo.setBounds(10, 11, 224, 33);
+		lblTipo.setForeground(new Color(0, 204, 102));
+		lblTipo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTipo.setFont(new Font("Agency FB", Font.PLAIN, 30));
 		contentPane.add(lblTipo);
 		
 		lblImagen = new JLabel("");
+		lblImagen.setBounds(10, 78, 224, 94);
 		lblImagen.setHorizontalAlignment(SwingConstants.CENTER);
-		lblImagen.setBounds(10, 55, 224, 153);
 		contentPane.add(lblImagen);
 		
-		txtrDescripcion = new JTextArea();
-		txtrDescripcion.setFont(new Font("Agency FB", Font.PLAIN, 20));
-		txtrDescripcion.setEditable(false);
-		txtrDescripcion.setBackground(new Color(51, 51, 51));
-		txtrDescripcion.setForeground(new Color(255, 255, 255));
-		txtrDescripcion.setText("Descripcion");
-		txtrDescripcion.setBounds(10, 219, 224, 74);
-		
 		JButton btnVolver = new JButton("Ok");
-		btnVolver.setBackground(new Color(255, 255, 255));
-		btnVolver.setFont(new Font("Agency FB", Font.ITALIC, 23));
-		btnVolver.setBounds(10, 328, 224, 37);
+		btnVolver.setIcon(new ImageIcon(FrameInfo.class.getResource("/Imagenes/accept1.png")));
+		btnVolver.setBounds(0, 363, 250, 37);
+		btnVolver.setForeground(new Color(255, 255, 255));
+		btnVolver.setBackground(new Color(42,154,255));
+		btnVolver.setFont(new Font("Agency FB", Font.PLAIN, 27));
 		btnVolver.addActionListener(this);
 		contentPane.add(btnVolver);
-		
+		txtrDescripcion = new JTextArea();
+		txtrDescripcion.setColumns(2);
+		txtrDescripcion.setFont(new Font("Agency FB", Font.PLAIN, 18));
+		txtrDescripcion.setEditable(false);
+		txtrDescripcion.setBackground(new Color(51,51,51));
+		txtrDescripcion.setForeground(new Color(0, 204, 153));
+		txtrDescripcion.setText("Descripcion");
+		txtrDescripcion.setLineWrap(true);
+		txtrDescripcion.setWrapStyleWord(true);
+		txtrDescripcion.setBounds(10, 200, 224, 147);
+		contentPane.add(txtrDescripcion);
 		JScrollPane scrollPane = new JScrollPane(txtrDescripcion);
-		scrollPane.setBounds(10, 219, 224, 74);
+		scrollPane.setBounds(10, 200, 224, 147);
+		scrollPane.setBorder(null);
 		contentPane.add(scrollPane);
 		
 		JLabel lblFondo = new JLabel(new ImageIcon(getClass().getResource("/Imagenes/Framefondo.png")));
 		lblFondo.setBounds(0, 0, 250, 400);
 		contentPane.add(lblFondo);
+		
+		
 	}
 
 	public JLabel getLblTipo() {
@@ -112,6 +112,11 @@ public class FrameInfo extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		this.setVisible(false);
+		if (fc != null) {
+			fc.setVisible(true);
+		}else
+		if (fp != null) {
+			fp.setVisible(true);
+		}
 	}
-	
 }
